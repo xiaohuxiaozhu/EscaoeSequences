11cd5066b38f6c4b2a67d1cdff126e9c1016f97a
GBTC441002UR <GBTC441002UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-06-13 13:32:28 -0400

    first commit

---
+++ src/EscapeSequences.java
@@ -0,0 +1,42 @@
+public class EscapeSequences {
+
+    public static void main(String []args) {
+
+
+      //  System.out.print("\t**");
+      //  System.out.print("\b++++");
+      //  System.out.print("\n*******");
+       // System.out.print("\r88888888");
+       // System.out.print("\f****");
+       // System.out.print("" + "\'" + "insert a single quote");
+        //     System.out.print("\"");
+        //
+        //     System.out.print(\);
+
+        //Write a Java program to print a box, an oval,
+        // or a diamond (choose one) using asterisks.
+
+        System.out.println("***** ");
+        System.out.println("*\t*");
+        System.out.println("*\t*");
+        System.out.println("*\t*");
+        System.out.println("*\t*");
+        System.out.println("***** ");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("   **");
+        System.out.println(" *   *");
+        System.out.println(" *   *");
+        System.out.println("  *  *");
+        System.out.println("   **");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("  *");
+        System.out.println(" * *");
+        System.out.println("*   *");
+        System.out.println(" * *");
+        System.out.println("  *");
+
+
+    }
+}
